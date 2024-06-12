63c6e9033254cfdea76d154f78502e8d8f7f6f97
tran <contact.mctdevelopment@gmail.com>
2024-06-12 10:32:37 +0200

    coment espace perso

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -74,9 +74,9 @@
                         <li class="nav-item">
                             <a href="#contact" class="nav-link">Contact</a>
                         </li>
-                        <li class="nav-item">
+<!--                        <li class="nav-item">
                             <a href="{{ route('login') }}" class="nav-link">Mon Espace</a>
-                        </li>
+                        </li> -->
                     </ul>
                 </div>
             </div>
